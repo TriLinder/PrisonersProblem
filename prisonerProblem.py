00f0b0ace51ce5b4c1255947f79f10e8334c5642
TriLinder <82846064+TriLinder@users.noreply.github.com>
2022-07-29 00:40:04 +0200

    Complete test() function

--- prisonerProblem.py
+++ prisonerProblem.py
@@ -1,12 +1,35 @@
 import random
 
 def test(prisoners) :
+    #Generate boxes
     boxes = []
 
     for i in range(prisoners) :
         boxes.append(i)
     
     random.shuffle(boxes)
-    print(boxes)
+    
+    #Simulate prisoners
+    sucess = True
+
+    for i in range(prisoners) :
+        foundMyBox = False
+        boxesSearched = []
+        target = i
+
+        while not foundMyBox and len(boxesSearched) <= prisoners / 2 :
+            boxesSearched.append(target)
+            
+            if boxes[target] == i : #Box found!
+                foundMyBox = True
+                #print(f"Prisoner {i} found their box.")
+            else :
+                target = boxes[target] #Find new target, continue in loop
+        
+        if not foundMyBox :
+            sucess = False
+            break
+    
+    return sucess
 
 test(100)
